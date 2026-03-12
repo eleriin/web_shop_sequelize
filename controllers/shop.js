cf90ea22d10f36c13786196073a3a3e46e6e110a
eleriin <eleriin.kurs@voco.ee>
2026-03-12 15:00:38 +0200

    Create carts table and manage carts data

--- controllers/shop.js
+++ controllers/shop.js
@@ -48,34 +48,29 @@ class shopController {
             res.status(500).json({error:'server error 1'})
         }
     }
-    async removeFromCart(req,res){
-        try{
+    async removeFromCart(req, res) {
+        try {
             const productId = parseInt(req.body.productId, 10);
-        if (isNaN(productId)) return res.status(400).json({ error: 'Vigane productId' });
+            if (isNaN(productId)) return res.status(400).json({ error: 'Vigane productId' });
 
-        const product = await Product.findByPk(productId);
-        if (!product)
-                return res.status(404).json({error: 'toodet ei leitud'})
-            const cart = await req.user.getCart()
-            if (!cart)
-                return res.status(500).json({error:'Ostukorvi ei leitud'})
-            
-            const items = await cart.getProducts({ where: {id: product.id}})
+            const product = await Product.findByPk(productId);
+            if (!product) return res.status(404).json({ error: 'Toodet ei leitud' });
 
-            if(items.length === 0){
-                return res.status(400).json({error: 'Toodet ei ole ostukorvis'})
+            const cart = await req.user.getCart();
+            if (!cart) return res.status(500).json({ error: 'Ostukorvi ei leitud' });
+
+            const items = await cart.getProducts({ where: { id: product.id } });
+
+            if (items.length === 0) {
+                return res.status(400).json({ error: 'Toodet ei ole ostukorvis' });
             }
 
-            const item = items[0]
-            if(item.cartItem.quantity > 1){
-                await item.cartItem.decrement('quantity', {by: 1})
-            } else {
-                await cart.removeProduct(product)
-            }
-            res.status(200).json({message: 'Toode on ostukorvist eemaldatud'})
-        } catch (err){
-            console.error(err)
-            res.status(500).json({ error:'server error 2'})
+            await cart.removeProduct(product);
+
+            res.status(200).json({ message: 'Toode on ostukorvist eemaldatud' });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'server error 2' });
         }
     }
 }
